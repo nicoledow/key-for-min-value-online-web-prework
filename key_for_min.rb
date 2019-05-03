8a439242c3538c11a1e66e54b8103392258a0c8a
Nicole Dow <nikki.dow71@gmail.com>
2019-05-03 18:45:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  key_of_smallest_value = ""
+  
+  name_hash.each do |key, value|
+    
+  end
+  key_of_smallest_value
 end
